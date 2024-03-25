4d4c525db7432c45d0ebda688a67b6f447d8c6cc
Lin, Ming Chih <mingchih.lin8@gmail.com>
2024-03-25 20:27:02 +0800

    0826

---
+++ assets/queryAEDT_API_simple.py
@@ -0,0 +1,145 @@
+import os
+import webbrowser
+
+version_name = oDesktop.GetVersion()
+folder = os.path.dirname(os.path.abspath(__file__))
+
+#%%
+
+html='''<!DOCTYPE html>
+<html>
+<head>
+    <style>
+    table.fixed {{
+        table-layout:fixed; 
+        }}
+    table.fixed td {{
+        overflow: hidden; 
+        }}
+    </style>
+</head>
+<body>{}</body>
+'''
+
+def returnTable(li,N):
+    x=''
+    n=0
+    for i in li:
+        if n==0:
+            x+='<tr>'
+        x+='<td>{}</td>'.format(i)
+        n+=1
+        if n==N:
+            x+='</tr>\n'
+            n=0
+    return '<table class="fixed">\n<col width="360px"/>\n<col width="360px"/>\n<col width="360px"/>{}</table>'.format(x)
+
+def generateHtml(funcs, html_name, color='red'):
+    html_path=os.path.join(folder, html_name)
+
+    x='<h1 style="color:{}";>{}</h1>'.format(color, html_name)
+    for i in funcs:
+        f_list = [j for j in funcs[i] if not j.startswith('_')] 
+        x+='<h2 style="color:{}";>{}({})</h2>'.format(color, i, len(f_list))
+        x+=returnTable(f_list,3)
+
+       
+    with open(html_path,'w') as sf:
+        sf.writelines(html.format(x))        
+    webbrowser.open('file://' + os.path.realpath(html_path))
+    
+#--------------------------------------------------------------------     
+   
+from collections import OrderedDict
+
+oProject = oDesktop.NewProject()
+oDefinitionManager = oProject.GetDefinitionManager()
+
+
+def exportFunctions(oDesign, oEditor):
+    data=OrderedDict()
+    data['oDesktop']=dir(oDesktop)
+    data['oProject']=dir(oProject)
+    data['oDefinitionManager']=dir(oDefinitionManager)
+    data['oDesign']=dir(oDesign)
+    data['oEditor']=dir(oEditor)
+
+    for m in ['Point', 'Polygon']:
+        try:
+            data ['oEditor-'+ m] = dir(getattr(oEditor, m)())
+        except:
+            pass
+    
+    for m in ["NdExplorer","ImportExport"]:
+        try:
+            data['oTool-'+m]= dir(oDesktop.GetTool(m))
+        except:
+            pass
+    
+    
+    for m in ["DataBlock","BoundarySetup","MeshSetup","AnalysisSetup","SimSetup","Optimetrics","Solutions","SolveSetups","ModelSetup","FieldsReporter","RadField","RadiationSetupMgr", "ReduceMatrix","ReportSetup","OutputVariable","UserDefinedSolutionModule","DV","Excitations","Padstack", "Cavities"]:
+        try:
+            data['oModule-' + m]= dir(oDesign.GetModule(m))
+        except:
+            pass
+    
+    for m in ["NdExplorer", "Component", "Material", "Model", "Symbol", "Footprint", "Padstack"]:
+        try:
+            data['oManager-' + m]= dir(oDefinitionManager.GetManager(m))
+        except:
+            pass
+            
+    for i in data:
+        data[i] = [j for j in data[i] if not j.startswith('__')]        
+  
+    return data
+
+
+#HFSS
+oDesign = oProject.InsertDesign("HFSS", "HFSSDesign1", "DrivenModal", "")
+oEditor = oDesign.SetActiveEditor("3D Modeler")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_HFSS.html'.format(version_name), 'OrangeRed')
+
+#3D Layout
+oDesign = oProject.InsertDesign("HFSS 3D Layout Design", "EMDesign1", "", "")
+oEditor = oDesign.SetActiveEditor("Layout")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_3D Layout.html'.format(version_name),'RoyalBlue')
+
+#Circuit
+oDesign = oProject.InsertDesign("Circuit Design", "Circuit1", "", "")
+oEditor = oDesign.SetActiveEditor("SchematicEditor")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_Circuit.html'.format(version_name),'SeaGreen')
+
+#Q3D
+oDesign=oProject.InsertDesign("Q3D Extractor", "Q3DDesign1", "", "")
+oEditor = oDesign.SetActiveEditor("3D Modeler")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_Q3D.html'.format(version_name),'Magenta')
+
+#Maxwell
+oDesign=oProject.InsertDesign("Maxwell 3D", "Maxwell3DDesign1", "Magnetostatic", "")
+oEditor = oDesign.SetActiveEditor("3D Modeler")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_Maxwell.html'.format(version_name),'DarkSalmon')
+
+#Icepak
+oDesign=oProject.InsertDesign("Icepak", "IcepakDesign2", "TemperatureAndFlow", "")
+oEditor = oDesign.SetActiveEditor("3D Modeler")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_Icepak.html'.format(version_name),'GoldenRod')
+
+#Mechanocal
+oDesign=oProject.InsertDesign("Mechanical", "MechanicalDesign1", "", "")
+oEditor = oDesign.SetActiveEditor("3D Modeler")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_Mechanical.html'.format(version_name),'MediumPurple')
+
+#Simplorer
+oDesign=oProject.InsertDesign("Twin Builder", "Simplorer1", "", "")
+oEditor = oDesign.SetActiveEditor("SchematicEditor")
+data=exportFunctions(oDesign, oEditor)
+generateHtml(data, '{}_Simplorer.html'.format(version_name),'Sienna')
+
---
+++ 腳本/輸出AEDT Classocal API HTML.md
@@ -0,0 +1,11 @@
+輸出AEDT Classocal API HTML
+---
+AEDT的Classical API在每個版本更新時都可能會有所變動，包括新增或刪除某些功能函數。然而，AEDT Scripting HELP文件的更新速度往往趕不上這些變化，導致開發者無法即時獲取最新的API資訊。為解決這個問題，可以利用一段腳本來探索並識別AEDT最新版本中的API變動。這段腳本能自動檢測並列出最新的API，並將結果輸出為HTML格式，方便開發者查詢。只需在AEDT環境中運行該腳本，便可獲得最新的API信息，從而提高開發效率和準確性。
+
+![2024-03-25_20-17-15](/assets/2024-03-25_20-17-15.png)
+
+![2024-03-25_20-25-53](/assets/2024-03-25_20-25-53.png)
+
+> :link: **腳本下載**
+[AEDT API函數HTML輸出](/assets//queryAEDT_API_simple.py)
+[AEDT API函數與參數HTML輸出](/assets/queryAEDT_API_signature.py)
